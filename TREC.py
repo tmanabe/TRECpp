dce51f9f84720c4f2c3dfba920d23bd3093e0082
Tomohiro Manabe <tmanabe@users.noreply.github.com>
2016-09-29 21:01:22 +0900

    add the Relevance class

--- TREC.py
+++ TREC.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from collections import defaultdict
+import re
+
 
 class Query(dict):
     linebreak = '\n'
@@ -9,8 +12,8 @@ class Query(dict):
     def read(self, path):
         with open(path, 'r') as file:
             for line in file:
-                key, value = line.split(self.separator, 1)
-                self[int(key)] = value.rstrip(self.linebreak)
+                query_id, query = line.split(self.separator, 1)
+                self[query_id] = query.strip()
         return self
 
     def write(self, path):
@@ -18,4 +21,39 @@ class Query(dict):
             for k in sorted(list(self.keys())):
                 file.write(self.separator.join([str(k), self[k]]))
                 file.write(self.linebreak)
+        return self
+
+class Relevance(dict):
+    linebreak = '\n'
+    separator = ' '
+
+    def __missing__(self, query_id):
+        self[query_id] = defaultdict(lambda:defaultdict(int))
+        return self[query_id]
+
+    def read(self, path):
+        with open(path, 'r') as file:
+            for line in file:
+                l = re.split('\\s+', line.strip(), 3)
+                query_id, intent_id, document_id, relevance = l
+                relevance = int(relevance)
+                self[query_id][intent_id][document_id] = relevance
+        return self
+
+    def write(self, path):
+        with open(path, 'w') as file:
+            for query_id in sorted(list(self.keys())):
+                from_i = self[query_id]
+                for intent_id in sorted(list(from_i.keys())):
+                    from_d = from_i[intent_id]
+                    for document_id in sorted(list(from_d.keys())):
+                        relevance = from_d[document_id]
+                        l = [
+                            query_id,
+                            intent_id,
+                            document_id,
+                            str(relevance),
+                        ]
+                        file.write(self.separator.join(l))
+                        file.write(self.linebreak)
         return self
